3542a3a1d9d3440314bf4553316a20cd2d4532e7
raphaelcaires <raphaelcaires@gmail.com>
2021-06-22 14:51:55 -0300

    versao 1.2

--- microdesafios/aula4_md1.js
+++ microdesafios/aula4_md1.js
@@ -1,7 +1,5 @@
 let status = false
 if (status) {
-  console.log("O valor é true - verdadeiro.")
+  return console.log("O valor é true - verdadeiro.")
 }
-else {
-  console.log("O valor é false - falso.")
-}
+return console.log("O valor é false - falso.")
